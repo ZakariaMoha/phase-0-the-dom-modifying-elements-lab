8db17ddc828c23915a90b58487933a5a010559ff
zakaria mohamed <zakmi.moha@gmail.com>
2023-10-02 11:15:09 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+ let x =document.getElementById("main")
+x.remove()
+const newHeader = document.createElement("h1")
+newHeader.textContent="YOUR-NAME is the champion"
+newHeader.setAttribute("id","victory")
+
+
+
